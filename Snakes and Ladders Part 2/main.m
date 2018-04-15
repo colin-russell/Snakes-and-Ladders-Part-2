e4034a9112a4306107fff55a24a6d1963b43d522
Colin Russell <colinrussell101@gmail.com>
2018-04-15 15:54:03 -0400

    added name property to Player class

--- Snakes and Ladders Part 2/main.m
+++ Snakes and Ladders Part 2/main.m
@@ -1,17 +1,40 @@
 //
 //  main.m
-//  Snakes and Ladders Part 2
+//  Snakes and Ladders
 //
 //  Created by Colin on 2018-04-15.
 //  Copyright © 2018 Colin Russell. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "Player.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        BOOL gameOver = NO;
+        Player *player = [Player new];
+        
+        NSLog(@"WELCOME TO SNAKES AND LADDERS! \nType \"roll\" or \"r\" to roll the die:");
+        
+        
+        while (gameOver == NO) {
+            
+            char str[100];
+            fgets (str, 100, stdin);
+            NSString *inputString = [[NSString alloc] initWithUTF8String:str];
+            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+            if ([inputString containsString:@"roll"] | [inputString containsString:@"r"]) {
+                [player roll];
+            }
+            
+            if (player.currentSquare == 100)
+                gameOver = YES;
+            
+        }
+        
+        
     }
     return 0;
 }
